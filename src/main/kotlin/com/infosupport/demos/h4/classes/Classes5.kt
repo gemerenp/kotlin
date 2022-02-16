bd9e98664e6679bbb07945ce4cff6b3e716291fa
Bram Janssens <bram.janssens@infosupport.com>
2022-02-16 14:19:55 +0100

    Class diagram added.

--- src/main/kotlin/com/infosupport/demos/h4/classes/Classes5.kt
+++ src/main/kotlin/com/infosupport/demos/h4/classes/Classes5.kt
@@ -1,9 +1,8 @@
 package com.infosupport.demos.h4.classes
 
-// Inheritance:
-// Visibility modifiers: public by default
-// not allowed: animateTwice is not open but final by default
-// override fun animateTwice() {}
+// Inheritance
+//  Visibility modifiers: public by default
+// See resources/img/buttons.png
 
 fun main() {
     val richButton = RichButton(1)
@@ -18,68 +17,58 @@ fun main() {
     gif.stopAnimating()
 
     val talkativeButton = TalkativeButton()
-    talkativeButton.speech()
-    talkativeButton.giveSpeech()
+    talkativeButton.speech()            // public
+    talkativeButton.moduleInternals()   // internal
+    talkativeButton.giveSpeech()        // internal
 }
 
 // - Open, final, and abstract
-
 //   See https://drek4537l1klr.cloudfront.net/jemerov/HighResolutionFigures/table_4-2.png
-
-open class RichButton(val x: Int) : Clickable {     // open: can be overridden
+open class RichButton(val x: Int) : Clickable { // open: can be overridden
     public final fun disable() {}       // public final by default: can’t be overridden
     open fun animate() {}               // open: can be overridden
-    /*final?*/ override fun click() {}  // overrides a member in a superclass or interface; open by default, if not marked final
+    open override fun click() {}        // overrides a member in a superclass or interface; open by default, if not marked final
 }
 
 class MyRichButton(myX: Int) : RichButton(myX) {
     // override fun disable() {}        // not allowed
-    override fun showOff() {
-        TODO("Not yet implemented")
-    }
+    override fun showOff() {}
+    internal fun moduleInternals() {}
 }
 
 abstract class Animated(val a: Int) {
     abstract fun animate()      // open by default, final not allowed
     open fun stopAnimating() {}
-    fun animateTwice() {}
+    fun animateTwice() = repeat(2) { animate() }
 }
 
 class Gif(anA: Int) : Animated(anA) {
-
-    override fun animate() {  // you have to implement abstract method, just as in Java
-        println("Animating...")
-    }
+    // you have to implement abstract method, just as in Java
+    override fun animate() = println("Animating...")
 
     // allowed: stopAnimating is open
-    override fun stopAnimating() {
-        println("Stopped")
-    }
+    override fun stopAnimating() = println("Stopped")
 }
 
-// internal: visible in a module, not outside it
+// Internal: visible in a module, not outside it, see CallInternals.kt
+// A module is a set of Kotlin files compiled at the same time in a given project, e.g.:
+//  - Maven module
+//  -
+// See https://kotlinlang.org/docs/visibility-modifiers.html#modules,
 internal open class TalkativeButton : Focusable {
     private fun yell() = println("Hey!")             // private
-
-    protected fun whisper() = println("Let's talk!") // protected
-    /*
-       PROTECTED:
-       In Kotlin, visibility rules are simple, and a protected member
-       is only visible in the class and its subclasses.
-       Also note that extension functions (like below) of a class don’t get
-       access to its private or protected members.
-    */
-    fun speech() {
-        // allowed:
-        yell()
-        whisper()
+    internal fun moduleInternals() = println("???")  // internal
+    protected fun whisper() = println("Let's talk!") // protected: only visible in the class and its subclasses, _not_ in package
+    fun speech() {                                   // public
+        // allowed here, but not allowed in extension function giveSpeech() below
+        yell();whisper()
     }
+
 }
 
-// must be internal too (or less visible): it's not allowed to reference internal TalkativeButton from a public function giveSpeech:
+// must be internal too (or less visible)
 internal fun TalkativeButton.giveSpeech() {
     // Not allowed: reference private or protected members from an internal function
-    // yell()
-    // whisper()
-    this.speech()
+    // yell(); whisper()
+    speech()
 }

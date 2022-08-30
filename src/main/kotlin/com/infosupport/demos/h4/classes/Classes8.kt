9ccd3859594e0e7a48fad88f2804a7e50893cce3
Jan-Jelle Kester <jan-jelle.kester@infosupport.com>
2022-08-30 12:44:31 +0200

    Improve class delegation example

--- src/main/kotlin/com/infosupport/demos/h4/classes/Classes8.kt
+++ src/main/kotlin/com/infosupport/demos/h4/classes/Classes8.kt
@@ -4,41 +4,42 @@ package com.infosupport.demos.h4.classes
 
 fun main() {
     // Suppose we want to implement a collection but only override the contains method. Rest is same as arraylist.
-    val list = listOf(4, 2, 42)
+    val list = listOf(4, 2)
 
-    val delegatingCollectionClassicStyle = DelegatingCollectionClassicStyle(list)
-    println(delegatingCollectionClassicStyle.isEmpty())
-    println(delegatingCollectionClassicStyle.contains(42))
+    val hitchhikerCollectionClassicStyle = HitchhikerCollectionClassicStyle(list)
+    println(hitchhikerCollectionClassicStyle.isEmpty())
+    println(hitchhikerCollectionClassicStyle.contains(42))
 
-    val delegatingCollection = DelegatingCollection(list)
-    println(delegatingCollection.isEmpty())
-    println(delegatingCollection.contains(42))
+    val hitchhikerCollection = HitchhikerCollection(list)
+    println(hitchhikerCollection.isEmpty())
+    println(hitchhikerCollection.contains(42))
 }
 
-class DelegatingCollectionClassicStyle<T>(
-    private val innerList: Collection<T> = arrayListOf()
-) : Collection<T> {
+// Delegation design pattern
+class HitchhikerCollectionClassicStyle(
+    private val inner: Collection<Int> = arrayListOf()
+) : Collection<Int> {
 
     // only this method is really overridden with additional behaviour
-    override fun contains(element: T): Boolean {
+    override fun contains(element: Int): Boolean {
         println("Checking if ${this.javaClass.simpleName} contains $element ...")
-        return innerList.contains(element)
+        return if (element == 42) true else inner.contains(element)
     }
 
     // these methods have to be explicitly overridden and they simply delegate to innerList; boilerplate code.
-    override val size: Int get() = innerList.size
-    override fun isEmpty() = innerList.isEmpty()
-    override fun iterator() = innerList.iterator()
-    override fun containsAll(elements: Collection<T>) = innerList.containsAll(elements)
+    override val size: Int get() = inner.size
+    override fun isEmpty() = inner.isEmpty()
+    override fun iterator() = inner.iterator()
+    override fun containsAll(elements: Collection<Int>) = inner.containsAll(elements)
 }
 
-class DelegatingCollection<T>(
-    private val innerList: Collection<T> = arrayListOf()
-) : Collection<T> by innerList { // note the `by` keyword
+class HitchhikerCollection(
+    private val inner: Collection<Int> = arrayListOf()
+) : Collection<Int> by inner { // note the `by` keyword
 
-    override fun contains(element: T): Boolean {
+    override fun contains(element: Int): Boolean {
         println("Checking if ${this.javaClass.simpleName} contains $element ...")
-        return innerList.contains(element)
+        return if (element == 42) true else inner.contains(element)
     }
 
     // all others are automatically overridden and they delegate to innerList!

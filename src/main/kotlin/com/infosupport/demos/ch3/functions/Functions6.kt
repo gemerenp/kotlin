18873f7e1d3ec624a8d38977707863ac35c0dcb4
Bram Janssens <s.a.janssens@gmail.com>
2022-12-07 12:56:13 +0100

    Small addition.

--- src/main/kotlin/com/infosupport/demos/ch3/functions/Functions6.kt
+++ src/main/kotlin/com/infosupport/demos/ch3/functions/Functions6.kt
@@ -8,20 +8,28 @@ fun main() {
 
     // use vararg
     print(array)
-    print(array, "A", "B")
-    print(array, "A", "B", "C", "D")
+    print(array, "", "A", "BB")
+    print(array, "", "A", "BB", "CCC", "DDDD")
 
     // You can spread the contents of a vararg with *, see fun print below.
     // But, you cannot spread the contents of a _list_ like this:
-    //   println(listOf("list spread: ", *list))
+    //   println(listOf("list spread", *list))
     // Instead you do it like this:
-    println(listOf("list spread: ", *list.toTypedArray()))
-
-    // More idiomatic is:
-    println(listOf("list spread: ") + list)
+    println(listOf("list spread", *list.toTypedArray()))
 }
 
-fun print(array: Array<Int>, vararg varargString: String) { // declare vararg parameter; in Java: String...letters
+fun print(array: Array<Int>, vararg varargString: String) { // declare vararg parameter; in Java: String... varargString
+
+    // vararg behaves like an array:
+    array.size
+    array[0]
+    array.binarySearch("A")
+
+    varargString.size
+    varargString[0]
+    varargString.binarySearch("A")
+    // etc.
+
 
     // You can pass the array, like in Java, but that leads to other results than in Java:
     println(listOf("array", array))
@@ -31,7 +39,20 @@ fun print(array: Array<Int>, vararg varargString: String) { // declare vararg pa
     println(listOf("*array", *array)) // spread unpacks the array contents
     println(listOf("*varargString", *varargString))
 
-    println()
+    println("Processing the varargString:")
+    // imperative style
+    for (s in varargString) {
+        if (s.isNotBlank()) {
+            val len = s.length * 2
+            println(len)
+        }
+    }
+
+    // functional style
+    varargString
+        .filter { s -> s.isNotBlank() }
+        .map { s -> s.length * 2 }
+        .forEach { println(it) }
 }
 
 // Lab 2d
